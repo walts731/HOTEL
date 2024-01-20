cd3710fffc8485b6b818ccd00b30d10b182d37b5
walts731 <142887725+walts731@users.noreply.github.com>
2024-01-20 19:26:11 +1000

    front-end index

--- index.php
+++ index.php
@@ -27,146 +27,7 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">WALTON'S HOTEL</a>
-        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="#">Rooms</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="#">Facilities</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="#">Contact Us</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="#">About</a>
-            </li>
-        </ul>
-        <div class="d-flex">
-            <button type="button" class="btn btn-outline-dark shadow-none mg-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-            Login
-            </button>
-            <button type="button" class="btn btn-outline-dark shadow-none mg-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal">
-            Register
-            </button>
-        </div>
-        </div>
-    </div>
-    </nav>
-
-
-    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form>
-                <div class="modal-header">
-                <h5 class="modal-title">
-                    <i class="bi bi-person-circle fs-3 me-2"></i> User Login
-                </h5>
-                <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control shadow-none">
-                    </div>
-                    <div class="mb-4">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between mb-2">
-                        <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-    </div>
-
-    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <form>
-                <div class="modal-header">
-                <h5 class="modal-title">
-                    <i class="bi bi-person-lines-fill"></i> User Registration
-                </h5>
-                <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <span class="badge rounded-pill text-bg-light mb-3 text-wrap lh-base">
-                        Note: Your details must match with your ID (National ID, passport, driving license etc.) that will be required during check-in.
-                    </span>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Name</label>
-                                <input type="email" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0">
-                            <label class="form-label ">Email</label>
-                                <input type="email" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Phone Number</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Picture</label>
-                                <input type="file" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-12 ps-0 mb-3">
-                            <label class="form-label ">Address</label>
-                                <textarea class="form-control shadow-none" rows="1"></textarea>
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Pincode</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Date of Birth</label>
-                                <input type="date" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Confirm Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center my-1">
-                        <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
-                    </div>
-                    <!-- <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control shadow-none">
-                    </div>
-                    <div class="mb-4">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between mb-2">
-                        <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
-                    </div> -->
-                </div>
-            </form>
-        </div>
-    </div>
-    </div>
+    <?php require('inc/heaader.php'); ?>
 
     <!-- carousel -->
     <div class="container-fluid">
@@ -439,10 +300,6 @@
                 <img src="images/features/wifi.png" width="80px">
                 <h5 class="mt-3">WiFi</h5>
             </div>
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/features/wifi.png" width="80px">
-                <h5 class="mt-3">WiFi</h5>
-            </div>
             <div class="col-lg-12 text-center mt-5">
                 <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities</a>
             </div>
@@ -452,27 +309,139 @@
     <!-- Testimonials  -->
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
 
-    <div class="container">
+    <div class="container mt-5">
         <!-- Swiper -->
         <div class="swiper swiper-testimonials">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class="profile">
-                        <img src="images/features/" alt="">
-                        <h6>Random User1</h6>
+            <div class="swiper-wrapper mb-5">
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.png" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
                     </div>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam impedit corrupti aperiam officia accusantium, voluptate explicabo beatae adipisci consequuntur dolor!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Laboriosam impedit corrupti aperiam officia accusantium, voluptate explicabo beatae adipisci consequuntur dolor!
                     </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.png" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Laboriosam impedit corrupti aperiam officia accusantium, voluptate explicabo beatae adipisci consequuntur dolor!
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/features/star.png" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Laboriosam impedit corrupti aperiam officia accusantium, voluptate explicabo beatae adipisci consequuntur dolor!
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
                 </div>
             </div>
             <div class="swiper-pagination"></div>
         </div>
+        <div class="col-lg-12 text-center mt-5">
+                <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More</a>
+        </div>
     </div>
-    
 
-    <br><br><br>
-    <br><br><br>
+    <!-- Reach Us  -->
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.86803545447!2d123.48468659999999!3d13.296182899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a1a0f8e33fed49%3A0x50cbb3fd2083cc1!2sBicol%20University%20Polangui!5e0!3m2!1sen!2sph!4v1705739575235!5m2!1sen!2sph" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Call Us</h5>
+                    <a href="tel: +917778889991" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+917778889991</a>
+                    <br>
+                    <a href="tel: +917778889991" class="d-inline-block text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+917778889991</a>
+                </div>
+
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Follow Us</h5>
+                    <a href="#" class="d-inline-block mb-3 text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-twitter me-1"></i> Twitter
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3 text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-facebook me-1"></i> Facebook
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-instagram me-1"></i> Instagram
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer  -->
+    <div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p4">
+                <h3 class="h-font fw-bold fs-3 mb-2">WALTON'S HOTEL</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Placeat iusto minima quas cupiditate quae non fugit. 
+                    Consequuntur, deleniti beatae? Odio officia natus laudantium velit placeat illo excepturi, esse veritatis quod?</p>
+            </div>
+            <div class="col-lg-4 p4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
+                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
+                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
+                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Contact Us</a><br>
+                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p4">
+                <h5 class="mb-3">Follow Us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration none mb-2 text-decoration-none"><i class="bi bi-twitter me-1"></i>Twitter</a><br>
+                <h5 class="mb-3">Follow Us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration none mb-2 text-decoration-none"><i class="bi bi-facebook me-1"></i>Facebook</a><br>
+                <h5 class="mb-3">Follow Us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration none text-decoration-none mb-2"><i class="bi bi-instagram me-1"></i>Instagram</a><br>
+            </div>
+        </div>
+    </div>
+
+    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by WALTON LONEZA</h6>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
@@ -493,16 +462,32 @@
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: "auto",
+        slidesPerView: "3",
+        loop: true,
         coverflowEffect: {
             rotate: 50,
             stretch: 0,
             depth: 100,
             modifier: 1,
-            slideShadows: true,
+            slideShadows: false,
         },
         pagination: {
             el: ".swiper-pagination",
         },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        }
         });
     </script>
 </body>
