f73c1d49e0c397afb0fdb8a3da529e02111e366e
walts731 <142887725+walts731@users.noreply.github.com>
2024-01-21 09:06:46 +1000

    facilities

---
+++ facilities.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Walton's Hotel - FACILITIES</title>
+    <?php require('inc/links.php'); ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <style>
+        .availability-form{
+            margin-top: -50px;
+            z-index: 2;
+            position: relative;
+        }
+
+        @media screen and (max-width: 575px){
+            .availability-form{
+            margin-top: 0px;
+            padding: 0 35px;
+            }
+        }
+
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+</head>
+<body>
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Commodi facilis porro necessitatibus eveniet quos <br> vel quam laudantium similique mollitia blanditiis possimus.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/features/wifi.png" width="40px">
+                    <h5 class="m-0 ms-3">WiFi</h5>
+                </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus reiciendis recusandae perspiciatis cum incidunt aspernatur fuga!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php'); ?>
+
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Walton's Hotel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Walton's Hotel - HOME</title>
+    <?php require('inc/links.php'); ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="css/common.css">
     <style>
         .availability-form{
             margin-top: -50px;
@@ -27,7 +22,7 @@
     </style>
 </head>
 <body>
-    <?php require('inc/heaader.php'); ?>
+    <?php require('inc/header.php'); ?>
 
     <!-- carousel -->
     <div class="container-fluid">
@@ -413,37 +408,8 @@
         </div>
     </div>
 
-    <!-- Footer  -->
-    <div class="container-fluid bg-white mt-5">
-        <div class="row">
-            <div class="col-lg-4 p4">
-                <h3 class="h-font fw-bold fs-3 mb-2">WALTON'S HOTEL</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Placeat iusto minima quas cupiditate quae non fugit. 
-                    Consequuntur, deleniti beatae? Odio officia natus laudantium velit placeat illo excepturi, esse veritatis quod?</p>
-            </div>
-            <div class="col-lg-4 p4">
-                <h5 class="mb-3">Links</h5>
-                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
-                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
-                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
-                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Contact Us</a><br>
-                <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">About</a>
-            </div>
-            <div class="col-lg-4 p4">
-                <h5 class="mb-3">Follow Us</h5>
-                <a href="#" class="d-inline-block text-dark text-decoration none mb-2 text-decoration-none"><i class="bi bi-twitter me-1"></i>Twitter</a><br>
-                <h5 class="mb-3">Follow Us</h5>
-                <a href="#" class="d-inline-block text-dark text-decoration none mb-2 text-decoration-none"><i class="bi bi-facebook me-1"></i>Facebook</a><br>
-                <h5 class="mb-3">Follow Us</h5>
-                <a href="#" class="d-inline-block text-dark text-decoration none text-decoration-none mb-2"><i class="bi bi-instagram me-1"></i>Instagram</a><br>
-            </div>
-        </div>
-    </div>
+    <?php require('inc/footer.php'); ?>
 
-    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by WALTON LONEZA</h6>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <!-- Initialize Swiper -->
     <script>
