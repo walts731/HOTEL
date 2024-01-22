1a85e6d8b6d68829a7df06b0cca1ba912e02fefd
walts731 <142887725+walts731@users.noreply.github.com>
2024-01-23 00:16:37 +1000

    adminloginpanel,dashboard,logout

---
+++ admin/dashboard.php
@@ -0,0 +1,23 @@
+<?php
+    require('inc/essentials.php');
+    adminLogin();
+    session_regenerate_id(true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - Dashboard</title>
+    <?php require('inc/links.php');?>
+<body class="bg-light">
+    
+    <div class="container-fluid bg-dark text-light p-3 d-flex align items-center justify-content-between">
+        <h3 class="mb-0">ADMIN PANEL</h3>
+        <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+    </div>
+
+
+    <?php require('inc/script.php'); ?>
+</body>
+</html>
---
+++ admin/inc/connect.php
@@ -0,0 +1,39 @@
+<?php
+    $hname = 'localhost';
+    $uname = 'root';
+    $pass = '12345';
+    $db = 'hotel';
+
+    $con = mysqli_connect($hname,$uname,$pass,$db);
+
+    if(!$con){
+        die("Hindi kumunekt sa database".mysqli_connect_error());
+    }
+
+    function filteration($data){
+        foreach($data as $key => $value){
+            $data[$key] = trim($value);
+            $data[$key] = stripslashes($value);
+            $data[$key] = htmlspecialchars($value);
+            $data[$key] = strip_tags($value);
+        }
+        return $data;
+    }
+
+    function select($sql,$values,$datatypes){
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql)){
+            mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+               $res = mysqli_stmt_get_result($stmt);
+               mysqli_stmt_close($stmt);
+               return $res;
+            }else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Select");
+            } 
+        }else{
+            die("Query cannot be prepared - Select");
+        }
+    }
+?>
