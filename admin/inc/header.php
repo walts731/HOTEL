2ca3da31405a09329376a0e96e235f34b68acb90
walts731 <142887725+walts731@users.noreply.github.com>
2024-01-23 11:28:36 +1000

    admin header

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,128 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">WALTON'S HOTEL</a>
-        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="rooms.php">Rooms</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="facilities.php">Facilities</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="contact.php">Contact Us</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="about.php">About</a>
-            </li>
-        </ul>
-        <div class="d-flex">
-            <button type="button" class="btn btn-outline-dark shadow-none mg-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-            Login
-            </button>
-            <button type="button" class="btn btn-outline-dark shadow-none mg-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal">
-            Register
-            </button>
-        </div>
-        </div>
-    </div>
-    </nav>
-
-
-    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form>
-                <div class="modal-header">
-                <h5 class="modal-title">
-                    <i class="bi bi-person-circle fs-3 me-2"></i> User Login
-                </h5>
-                <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+<div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid flex-lg-column align-items-stretch">
+                <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                    <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Rooms</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="settings.php">Settings</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control shadow-none">
-                    </div>
-                    <div class="mb-4">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between mb-2">
-                        <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-    </div>
-
-    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <form>
-                <div class="modal-header">
-                <h5 class="modal-title">
-                    <i class="bi bi-person-lines-fill"></i> User Registration
-                </h5>
-                <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <span class="badge rounded-pill text-bg-light mb-3 text-wrap lh-base">
-                        Note: Your details must match with your ID (National ID, passport, driving license etc.) that will be required during check-in.
-                    </span>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Name</label>
-                                <input type="email" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0">
-                            <label class="form-label ">Email</label>
-                                <input type="email" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Phone Number</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Picture</label>
-                                <input type="file" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-12 ps-0 mb-3">
-                            <label class="form-label ">Address</label>
-                                <textarea class="form-control shadow-none" rows="1"></textarea>
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Pincode</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Date of Birth</label>
-                                <input type="date" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                            <label class="form-label ">Confirm Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center my-1">
-                        <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+            </div>
+        </nav>
     </div>
