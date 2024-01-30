da4428cc5c017a840cc23e5834a34a466dcfb804
walts731 <142887725+walts731@users.noreply.github.com>
2024-01-30 19:44:05 +1000

    management team section

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,26 +1,68 @@
 <?php
 
+    //frontend purpose data
+
+    define('SITE_URL','http://127.0.0.1/HOTE/');
+    define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
+
+    //backend upload process needs this data
+
+    define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/HOTEL/images/');
+    define('ABOUT_FOLDER', 'about/');
+
     function adminLogin(){
         session_start();
-        if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
-            echo"<script>window.location.href='index.php';</script>";
-            exit;
+        if (!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)){
+            redirect('index.php');
         }
     }
 
     function redirect($url){
-        echo"<script>window.location.href='$url';</script>";
+        header("Location: $url");
         exit;
     }
 
-    function alert($type,$msg){
+    function alert($type, $msg){
         $bs_class = ($type == "success") ? "alert-success" : "alert-danger";
 
         echo <<<alert
-                    <div class="alert $bs_class alert-dismissible fade show custom-alert" role="alert">
-                        <strong class="me-3">$msg</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                alert;
+            <div class="alert $bs_class alert-dismissible fade show custom-alert" role="alert">
+                <strong class="me-3">$msg</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        alert;
+    }
+
+    function uploadImage($image, $folder){
+        $valid_mime = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
+
+        $img_mime = $image['type'];
+        $img_extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+
+        if (!in_array($img_mime, $valid_mime)) {
+            return 'inv_img'; // Invalid image mime or format
+        } elseif (($image['size'] / 1024 * 1024) > 2) {
+            return 'inv_size'; // Invalid size greater than 2MB
+        } else {
+            $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+            $rname = 'IMG_' . random_int(11111, 99999) . ".$ext";
+
+            $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
+
+            if (move_uploaded_file($image['tmp_name'], $img_path)) {
+                return $rname;
+            } else {
+                return 'upd_failed';
+            }
+        }
+    }
+
+    function deleteImage($image, $folder){
+        
+        if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+            return true;
+        }else{
+            return false;
+        }
     }
 ?>
--- admin/settings.php
+++ admin/settings.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Settings</title>
     <?php require('inc/links.php');?>
+</head>
 <body class="bg-light">
     
     <div class="container-fluid bg-dark text-light p-3 d-flex align items-center justify-content-between sticky-top">
@@ -209,6 +210,47 @@
                     </div>
                 </div>
 
+                <!-- management team section -->
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h5 class="card-title m-0">Management Team</h5>
+                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#team-s"><i class="bi bi-plus-square"></i>Add</button>
+                        </div>
+                        
+                        <div class="row" id="team-data">
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <!-- management team modal -->
+                <div class="modal fade" id="team-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form id="team_s_form">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title mb-3">Add Team Member</h5>
+                            </div>
+                            <div class="modal-body">
+                            <div class="mb-3">
+                                    <label class="form-label fw-bold mt-3">Name</label>
+                                    <input type="text" name="member_name" id="member_name_inp" class="form-control shadow-none" required>
+                            </div>
+                            <div class="mb-3">
+                                    <label class="form-label fw-bold">Picture</label>
+                                    <input type="file" name="member_picture" id="member_picture_inp" accept="[.jpg, .png, .webp, .jpeg]" class="form-control shadow-none" required>
+                            </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" onclick="member_name.value='', member_picture.value=''" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                                <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                            </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -222,6 +264,10 @@
         let site_about_inp = document.getElementById('site_about_inp');
 
         let contacts_s_form = document.getElementById('contacts_s_form');
+
+        let team_s_form = document.getElementById('team_s_form');
+        let member_name_inp = document.getElementById('member_name_inp');
+        let member_picture_inp = document.getElementById('member_picture_inp');
 
         function get_general(){
             let site_title = document.getElementById('site_title');
@@ -270,7 +316,7 @@
                 var modal = bootstrap.Modal.getInstance(myModal);
                 modal.hide();
 
-                if(this.reponseText == 1){
+                if(this.responseText == 1){
                     alert('success','Changes saved!');
                     get_general();
                 }else{
@@ -329,10 +375,10 @@
             }
         }
 
-        contacts_s_form.addEventListener('submit',function(e)){
+        contacts_s_form.addEventListener('submit',function(e){
             e.preventDefault();
             upd_contacts();
-        };
+        });
 
         function upd_contacts(){
             let index = ['address','gmail','pn1','pn2','email','fb','insta','tw','iframe'];
@@ -364,9 +410,76 @@
             xhr.send(data_str);
         }
 
+        team_s_form.addEventListener('submit',function(e){
+            e.preventDefault();
+            add_member();
+        });
+
+        function add_member(){
+            let data = new FormData();
+            data.append('name',member_name_inp.value);
+            data.append('picture',member_picture_inp.files[0]);
+            data.append('add_member','');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/settings_crud.php",true);
+
+
+            xhr.onload = function(){
+                var myModal = document.getElementById('team-s');
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+
+                if(this.responseText == 'inv_img'){
+                    alert('error','Only JPG and PNG images are allowed!');
+                }else if(this.responseText == 'inv_size'){
+                    alert('error','Image should be less than 2MB');
+                }else if(this.responseText == 'upd_failed'){
+                    alert('error','Image upload failed. Server Down!');
+                }else{
+                    alert('success','New member added!');
+                    member_name_inp.value = '';
+                    member_picture_inp.value = '';
+                    get_members();
+                }
+            }
+
+            xhr.send(data);
+        }
+
+        function get_members(){
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/settings_crud.php",true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function(){
+                document.getElementById('team-data').innerHTML = this.reponseText;
+            }
+
+            xhr.send('get_members');
+        }
+
+        function rem_member(val){
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/settings_crud.php",true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function(){
+                if(this.responseText==1){
+                    alert('success','Member removed!');
+                    get_members();
+                }else{
+                    ('error','$server down!');
+                }
+            }
+
+            xhr.send('rem_member='+val);
+        }
+
         window.onload = function(){
             get_general();
             get_contacts();
+            get_members();
         }
     </script>
 
